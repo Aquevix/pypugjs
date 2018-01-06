c08a5ee0da3d7cfae21c653c037e6cae789fb2e8
n! <ruzovej@cumacek.xyz>
2018-01-06 09:33:31 +0100

    Added: setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+from distutils.core import setup
+
+setup(
+    name="pypugjs",
+    version="0.1",
+    description="Fork of original and deleted https://github.com/matannoam/pypugjs",
+    author="Pavel Hrdina",
+)
